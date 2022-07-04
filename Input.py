c581447cd8b9d8a8fa4565f657977d9bd71a0243
SYCHORNLOADER <89072698+KARLOF9912@users.noreply.github.com>
2022-07-04 13:22:22 +0700

    Input.py

---
+++ Input.py
@@ -0,0 +1,14 @@
+#จากนนพัดนะน้าบ
+
+Number = int(input("กรอกคะแนนของคัณ :"))
+
+if Number > 79 :
+  Print("เกรดของคุณคือ 4")
+elif Number > 69 :
+  Print("เกรดของคุณคือ 3")
+elif Number > 59 :
+  Print("เกรดของคุณคือ 2")
+elif Number > 49: 
+  Print("เกรดของคุณคือ 1")
+elif Number < 50 : 
+  Print("เกรดของคุณคือ 0")
